c7711a1f833feb53be0ee462bcbad7f09edb9057
Marcão <o_marcaoo@outlook.com>
2026-03-13 09:31:36 -0300

    Create Exercicio 14.py

---
+++ Exercicio 14.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 13 09:22:12 2026
+
+@author: Marcão
+"""
+
+print("***INDICADOR DE NÚMEROS IGUAIS***")
+n = int(input("Digite um valor a ser testado: "))
+
+y = False
+
+while n !=0:
+    a = n % 10
+    n = n // 10
+    b = n % 10
+    if a == b:
+        print(f"O número {a} , possui dois digitos adjacentes iguais. ")
+        y = True
+        
+if y != True:
+    print("Não a números adjacentes iguais no valor digitado")
