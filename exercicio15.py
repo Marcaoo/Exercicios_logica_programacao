9455ab5aec9c67a52b5a3aee5519fdca0f0a7a4a
Marcão <o_marcaoo@outlook.com>
2026-03-31 09:15:54 -0300

    Create exercicio15.py

---
+++ exercicio15.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 09:09:58 2026
+
+@author: Marcão
+"""
+
+from math import factorial
+def Fatorial (n , p):
+   return factorial(n) / (factorial(p) * factorial(n-p))
+n = 1
+p = 1
+while n != 0 or p != 0:
+    try:
+        n = int(input('Digite o numerador binominal: '))
+        p = int(input('Digite o denominador binominal: '))
+        print(Fatorial(n, p))
+    except ValueError:
+        if p > n:
+            print('O denominador binominal não pode ser maior que o numerador binominal')
+        else:
+            print('Carácter inválido, tente novamente')
+print('Fim do exercício')
